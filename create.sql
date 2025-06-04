b9007b7a1befcb5b5edc114b9c4107f1a13f3be4
ana <anaisasena91@hotmail.com>
2025-06-04 23:50:28 +0100

    Fixed primary and foreigner keys

--- create.sql
+++ create.sql
@@ -4,21 +4,20 @@ USE lab_mysql;
 
 DROP TABLE IF EXISTS Cars;
 
- CREATE TABLE Cars (VIN CHAR(20), manufacture VARCHAR(20),
+ CREATE TABLE Cars (VIN CHAR(20) PRIMARY KEY, manufacturer VARCHAR(20),
        model VARCHAR(20), year YEAR, color CHAR(10));
 
 DROP TABLE IF EXISTS Customers;
 
- CREATE TABLE Customers (customer_ID CHAR(5), name VARCHAR(40), phone_number CHAR(20),
+ CREATE TABLE Customers (customer_ID CHAR(5) PRIMARY KEY, name VARCHAR(40), phone_number CHAR(20),
        email VARCHAR(30), address VARCHAR(40), city CHAR(20), state_province CHAR(20), country VARCHAR(30), zip_postal_code CHAR(10));
  
 DROP TABLE IF EXISTS Salesperson;
 
- CREATE TABLE Salesperson (staff_ID CHAR(5), name VARCHAR(40),
+ CREATE TABLE Salesperson (staff_ID CHAR(5) PRIMARY KEY, name VARCHAR(40),
        store CHAR(20));
      
   DROP TABLE IF EXISTS Invoices;
   
- CREATE TABLE Invoices (invoice_number CHAR(10), customer_ID CHAR(5),
-       staff_ID CHAR(5), VIN CHAR(20), date DATE, car CHAR(10), customer VARCHAR(40), salesperson VARCHAR(40) );
-
+ CREATE TABLE Invoices  ( invoice_number CHAR(10) PRIMARY KEY, customer_ID CHAR(5), staff_ID CHAR(5), VIN CHAR(20), date DATE, FOREIGN KEY (customer_ID) REFERENCES Customers(customer_ID), FOREIGN KEY (staff_ID) REFERENCES Salesperson(staff_ID),FOREIGN KEY (VIN) REFERENCES Cars(VIN)
+);
--- seeding.sql
+++ seeding.sql
@@ -1,10 +1,10 @@
-INSERT INTO Cars ( VIN, manufacture, model, year, color)
-VALUES ('3K096I98581DHSNUP', 'Volkswagen','Tiguan','2019','Blue'),
-('ZM8G7BEUQZ97IH46V', 'Peugeot','Rifter','2019','Red'),
-('RKXVNNIHLVVZOUB4M','Ford','Fusion','2018','White'),
-('HKNDGS7CU31E9Z7JW','Toyota','RAV4','2018','Silver'),
-('DAM41UDN3CHU2WVF6','Volvo','V60',	'2019','Gray'),
-('DAM41UDN3CHU2WVF6','Volvo','V60 Cross Country','2019','Gray');
+INSERT INTO Cars ( VIN, manufacturer, model, year, color)
+VALUES ('3K096I98581DHSNUP', 'Volkswagen','Tiguan',2019,'Blue'),
+('ZM8G7BEUQZ97IH46V', 'Peugeot','Rifter',2019,'Red'),
+('RKXVNNIHLVVZOUB4M','Ford','Fusion',2018,'White'),
+('HKNDGS7CU31E9Z7JW','Toyota','RAV4',2018,'Silver'),
+('DAM41UDN3CHU2WVF6','Volvo','V60',	2019,'Gray'),
+('DAM41UDN3CHU2WVF6','Volvo','V60 Cross Country',2019,'Gray');
        
  INSERT INTO Customers (customer_ID, name, phone_number, email, address, city, state_province, country, zip_postal_code)
 VALUES 	('10001','Pablo Picasso','+34 636 17 63 82','-','Paseo de la Chopera 14','Madrid','Madrid', 'Spain','28045'),
